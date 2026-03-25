87d791c999e7a6838c529ca74897da2c1c3f4a62
leoserodio <leoserodio06@gmail.com>
2026-03-25 15:32:39 -0400

    Update chip_interface.sv

--- chip_interface.sv
+++ chip_interface.sv
@@ -31,6 +31,92 @@ module ChipInterface (
     // - 2Declare connecting wires/signals to/from your game
     // - Use those wires/signals to connect to the MastermindVGA and
     //   EightSevenSegmentDisplays modules below
+        logic [11:0] guess, masterPattern;
+    logic [3:0]  znarly, zood, credit, roundNumber, numGames;
+
+    logic        master_ready;
+    logic        enough, space, max_rounds, more_rounds, correct;
+    logic        round_clear, cl_all, inc_game_fsm, adding_fsm, cl_z, round_en, gameWon;
+    logic        inc_game_coin, adding_coin;
+    logic        gradeIt;
+
+    logic        loadGuess, loadNumGames, loadZnarlyZood;
+    logic        clearGame, displayMasterPattern;
+
+    assign guess                = SW[11:0];
+    assign gradeIt              = BTN[3];
+    assign loadGuess            = BTN[3];
+    assign loadZnarlyZood       = BTN[3];
+    assign loadNumGames         = inc_game_coin;
+    assign clearGame            = reset_sync;
+    assign displayMasterPattern = gameWon | max_rounds;
+
+    grader GRADER (
+        .guess(guess),
+        .znarly(znarly),
+        .zood(zood),
+        .GradeIt(gradeIt),
+        .reset(reset_sync),
+        .clock(clk_40MHz)
+    );
+
+    Select_Pattern SELECT_PATTERN (
+        .ShapeLocation(SW[4:3]),
+        .LoadShape(SW[2:0]),
+        .guess(guess),
+        .cl_all(cl_all),
+        .LoadShapeNow(BTN[3]),
+        .clock(clk_40MHz),
+        .reset_L(~reset_sync),
+        .master_ready(master_ready),
+        .masterPattern(masterPattern)
+    );
+
+    gameCounter GAME_COUNTER (
+        .clock(clk_40MHz),
+        .inc_game(inc_game_coin),
+        .game_clear(reset_sync),
+        .inc_round(round_en),
+        .round_clear(round_clear),
+        .num_rounds(4'd8),
+        .znarly(znarly),
+        .enough(enough),
+        .space(space),
+        .max_rounds(max_rounds),
+        .more_rounds(more_rounds),
+        .correct(correct),
+        .roundNumber(roundNumber),
+        .numGames(numGames)
+    );
+
+    FSM CONTROL (
+        .clock(clk_40MHz),
+        .reset_L(~reset_sync),
+        .startGame(BTN[2]),
+        .enough(enough),
+        .master_ready(master_ready),
+        .gradeIt(gradeIt),
+        .correct(correct),
+        .more_rounds(more_rounds),
+        .max_rounds(max_rounds),
+        .round_clear(round_clear),
+        .cl_all(cl_all),
+        .inc_game(inc_game_fsm),
+        .adding(adding_fsm),
+        .cl_z(cl_z),
+        .round_en(round_en),
+        .gameWon(gameWon)
+    );
+
+    coinFSM COIN_FSM (
+        .credit(credit),
+        .inc_game(inc_game_coin),
+        .adding(adding_coin),
+        .cv(SW[15:14]),
+        .coinInserted(BTN[1]),
+        .clock(clk_40MHz),
+        .reset_L(~reset_sync)
+    );
 
 /*
  *  BEWARE CHANGING CODE BELOW THIS LINE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -56,42 +142,44 @@ module ChipInterface (
                         .VGA_VS,
                         .VGA_HS,
                         .reset(reset_sync),
-                        .numGames( ... ),
-                        .loadNumGames( ... ),
-                        .roundNumber( ... ),
-                        .guess( ... ),
-                        .loadGuess( ... ),
-                        .znarly( ... ), 
-                        .zood( ... ), 
-                        .clearGame( ... ),
-                        .masterPattern( ... ),
-                        .displayMasterPattern( ... ),
-			.loadZnarlyZood( ... )
+                        .numGames(numGames),
+                        .loadNumGames(loadNumGames),
+                        .roundNumber(roundNumber),
+                        .guess(guess),
+                        .loadGuess(loadGuess),
+                        .znarly(znarly), 
+                        .zood(zood), 
+                        .clearGame(clearGame),
+                        .masterPattern(masterPattern),
+                        .displayMasterPattern(displayMasterPattern),
+                        .loadZnarlyZood(loadZnarlyZood)
                        );
 
-
-    EightSevenSegmentDisplays displays(.HEX7( ... ), 
-                                       .HEX6( ... ), 
-                                       .HEX5( ... ), 
-                                       .HEX4( ... ),
-                                       .HEX3( ... ), 
-                                       .HEX2( ... ), 
-                                       .HEX1( ... ), 
-                                       .HEX0( ... ),
+    EightSevenSegmentDisplays displays(.HEX7(4'h0), 
+                                       .HEX6(4'h0), 
+                                       .HEX5(4'h0), 
+                                       .HEX4(4'h0),
+                                       .HEX3(znarly), 
+                                       .HEX2(zood), 
+                                       .HEX1(roundNumber), 
+                                       .HEX0(numGames),
                                        .CLOCK_100,
-                                       .reset( ... ),
-                                       .dec_points( ... ),
-                                       .blank( ... ),  
+                                       .reset(reset_sync),
+                                       .dec_points(8'b0),
+                                       .blank(8'b0),  
                                        .D2_AN,
                                        .D1_AN,
                                        .D2_SEG,
                                        .D1_SEG
                                       );
 
-    Synchronizer sync_reset(.async( ... ), 
+    Synchronizer sync_reset(.async(BTN[0]), 
                             .clock(clk_40MHz), 
                             .sync(reset_sync)
                            );
+
+    assign LD[0]    = gameWon;
+    assign LD[15:1] = 15'b0;
 
 /*
  *  DO NOT EDIT CODE BELOW THIS LINE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
